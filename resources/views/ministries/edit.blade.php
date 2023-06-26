23abc4cd998fcd521991da9ea43bf8b743373748
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-06-26 13:05:27 +0600

    Добавлены исправления.

--- resources/views/ministries/edit.blade.php
+++ resources/views/ministries/edit.blade.php
@@ -28,7 +28,7 @@
 
                         <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                             <div class="sm:col-span-2">
-                                <label for="ministry_name" class="block mb-4 text-md font-medium text-gray-900 dark:text-black">Введите новое название министерства</label>
+                                <label for="ministry_name" class="block mb-4 text-md font-medium text-gray-900 dark:text-black">Введите новое название министерства (либо введите старое, если не меняете название)</label>
                                 <input type="text" name="ministry_name" id="ministry_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Введите новое название министерства">
                             </div>
                         </div>
