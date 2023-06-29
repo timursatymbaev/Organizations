6e8e741f539894bbef43aba5b0a63171873726a0
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-06-29 21:36:34 +0600

    Рефакторинг.

--- resources/views/organizations/search.blade.php
+++ resources/views/organizations/search.blade.php
@@ -12,8 +12,17 @@
                         <form method="get">
                             <div class="flex justify-center">
                                 <input name="search" type="search" id="search"
-                                       class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       class="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                        placeholder="Найдите организацию по названию">
+                                <select name="filter" id="filter" class="ml-2 py-6 border border-gray-300 rounded-lg">
+                                    <option value="">Все</option>
+                                    <option value="Министерство">Министерства</option>
+                                    <option value="Комитет">Комитеты</option>
+                                    <option value="Управление">Управления</option>
+                                </select>
+                                <x-primary-button type="submit" class="ml-2 py-4">
+                                    <a href="{{ route('organizations.index') }}" class="p-2 bg-gray-300 text-white rounded-lg">Сбросить</a>
+                                </x-primary-button>
                                 <x-primary-button type="submit" class="ml-2 py-4">Найти</x-primary-button>
                             </div>
                         </form>
@@ -40,96 +49,47 @@
                             </tr>
                             </thead>
                             <tbody>
-                            @foreach($ministries as $ministry)
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black">
-                                        {{ $ministry->ministry_name }}
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        Министерство
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        Отсутствует
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <x-primary-button>
-                                            <a href="{{ route('ministries.show', $ministry->id) }}"
-                                               class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Просмотреть</a>
-                                        </x-primary-button>
-                                        <x-primary-button class="mt-1">
-                                            <a href="{{ route('ministries.edit', $ministry->id) }}"
-                                               class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Изменить</a>
-                                        </x-primary-button>
-                                        <form class="mt-1" action="{{ route('ministries.destroy', $ministry->id) }}"
-                                              method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <x-primary-button type="submit">Удалить</x-primary-button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            @foreach($committees as $committee)
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black">
-                                        {{ $committee->committee_name }}
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        Комитет
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $committee->ministry->ministry_name ?? 'Отсутствует' }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <x-primary-button>
-                                            <a href="{{ route('committees.show', $committee->id) }}"
-                                               class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Просмотреть</a>
-                                        </x-primary-button>
-                                        <x-primary-button class="mt-1">
-                                            <a href="{{ route('committees.edit', $committee->id) }}"
-                                               class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Изменить</a>
-                                        </x-primary-button>
-                                        <form class="mt-1" action="{{ route('committees.destroy', $committee->id) }}"
-                                              method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <x-primary-button type="submit">Удалить</x-primary-button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            @foreach($managements as $management)
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black">
-                                        {{ $management->management_name }}
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        Управление
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ $management->committee->committee_name ?? 'Отсутствует' }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <x-primary-button>
-                                            <a href="{{ route('managements.show', $management->id) }}"
-                                               class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Просмотреть</a>
-                                        </x-primary-button>
-                                        <x-primary-button class="mt-1">
-                                            <a href="{{ route('managements.edit', $management->id) }}"
-                                               class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Изменить</a>
-                                        </x-primary-button>
-                                        <form class="mt-1" action="{{ route('managements.destroy', $management->id) }}"
-                                              method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <x-primary-button type="submit">Удалить</x-primary-button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                @foreach($organizations as $organization)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black">
+                                            {{ $organization->name }}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $organization->type }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            @if($organization->type === 'Управление')
+                                                {{ $organization->followedByCommittee ? $organization->followedByCommittee->name : 'Отсутствует' }}
+                                            @else
+                                                {{ $organization->followedBy ? $organization->followedBy->name : 'Отсутствует' }}
+                                            @endif
+                                        </td>
+                                        @if(\Illuminate\Support\Facades\Auth::id() === $organization->created_by)
+                                            <td class="px-6 py-4">
+                                                <x-primary-button>
+                                                    <a href="{{ route('organizations.show', $organization->id) }}"
+                                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Просмотреть</a>
+                                                </x-primary-button>
+                                                <x-primary-button class="mt-1">
+                                                    <a href="{{ route('organizations.edit', $organization->id) }}"
+                                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Изменить</a>
+                                                </x-primary-button>
+                                                <form class="mt-1"
+                                                      action="{{ route('organizations.destroy', $organization->id) }}"
+                                                      method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <x-primary-button type="submit">Удалить</x-primary-button>
+                                                </form>
+                                            </td>
+                                        @else
+                                            <td class="px-6 py-4">
+                                                <h1>У вас нет доступа к данной организации.</h1>
+                                            </td>
+                                        @endif
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
