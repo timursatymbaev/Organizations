326c6b6130e1d90880459ff51b365a85588b30d4
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-06-29 21:35:05 +0600

    Рефакторинг.

--- app/Http/Requests/OrganizationRequest.php
+++ app/Http/Requests/OrganizationRequest.php
@@ -36,10 +36,10 @@ class OrganizationRequest extends FormRequest
                 'id' => ['required'],
                 'name' => ['required', 'max:255', 'string', 'unique:organizations,name'],
                 'type' => ['required'],
-                'followed_by_add' => [],
-                'followed_by_remove' => [],
-                'followed_by_committee_add' => [],
-                'followed_by_committee_remove' => [],
+                'followed_by_remove' => ['required_if:followed_by_add,null',],
+                'followed_by_add' => ['required_if:followed_by_remove,null',],
+                'followed_by_committee_remove' => ['required_if:followed_by_committee_add,null'],
+                'followed_by_committee_add' => ['required_if:followed_by_committee_remove,null'],
             ],
             default => []
         };
@@ -53,8 +53,12 @@ class OrganizationRequest extends FormRequest
             'name.string' => 'Название организации должно являться строкой.',
             'name.unique' => 'Организация с таким названием уже существует.',
             'type.required' => 'Тип организации является обязательным.',
-            'followed_by_remove.required_if', 'followed_by_add.required_if' => 'Выбор курирующей организации является обязательным.',
-            'followed_by_committee.required_if' => 'Выбор курирующего комитета является обязательным.'
+            'followed_by.required_if' => 'Выбор курирующего министерства является обязательным.',
+            'followed_by_remove.required_if' => 'Выбор курирующего министерства является обязательным.',
+            'followed_by_add.required_if' => 'Выбор курирующего министерства является обязательным.',
+            'followed_by_committee.required_if' => 'Выбор курирующего комитета является обязательным.',
+            'followed_by_committee_add.required_if' => 'Выбор курирующего комитета является обязательным.',
+            'followed_by_committee_remove.required_if' => 'Выбор курирующего комитета является обязательным.',
         ];
     }
 }
