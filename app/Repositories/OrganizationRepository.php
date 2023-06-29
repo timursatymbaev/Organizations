f130b26d2f84e236993acbb7765ae54514a3b26b
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-06-29 11:31:43 +0600

    Рефакторинг.

---
+++ app/Repositories/OrganizationRepository.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Organization;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class OrganizationRepository
+{
+    public function getOrganizations(): Collection
+    {
+        return Organization::all();
+    }
+
+    public function getFollowedByOrganizations(): Collection
+    {
+        return Organization::with('followedBy')->get();
+    }
+
+    public function createOrganization(array $data): Organization
+    {
+        return DB::transaction(function () use ($data) {
+            $type = $data['type'];
+
+            return $this->createOrganizationByType($type, $data);
+        });
+    }
+
+    private function createOrganizationByType(string $type, array $data): Organization
+    {
+        return match ($type) {
+            'Министерство' => $this->createMinistry($data),
+            'Комитет' => $this->createCommittee($data),
+            'Управление' => $this->createManagement($data),
+            default => null,
+        };
+    }
+
+    private function createMinistry(array $data): Organization
+    {
+        return Organization::create([
+            'name' => $data['name'],
+            'type' => 'Министерство',
+            'created_by' => Auth::id(),
+        ]);
+    }
+
+    private function createCommittee(array $data): Organization
+    {
+        return Organization::create([
+            'name' => $data['name'],
+            'type' => 'Комитет',
+            'created_by' => Auth::id(),
+            'followed_by' => $data['followed_by']
+        ]);
+    }
+
+    private function createManagement(array $data): Organization
+    {
+        return Organization::create([
+            'name' => $data['name'],
+            'type' => 'Управление',
+            'created_by' => Auth::id(),
+            'followed_by' => $data['followed_by'],
+            'followed_by_committee' => $data['followed_by_committee']
+        ]);
+    }
+
+    public function updateOrganization(array $data): Organization
+    {
+        return DB::transaction(function () use ($data) {
+            $type = $data['type'];
+
+            return $this->updateOrganizationByType($type, $data);
+        });
+    }
+
+    private function updateOrganizationByType(string $type, array $data): Organization
+    {
+        return match ($type) {
+            'Министерство' => $this->updateMinistry($data),
+            'Комитет' => $this->updateCommittee($data),
+            'Управление' => $this->updateManagement($data),
+            default => null
+        };
+    }
+
+    private function updateMinistry(array $data): Organization
+    {
+        $organization = Organization::findOrFail($data['id']);
+
+        $organization->name = $data['name'];
+        $organization->save();
+
+        return $organization;
+    }
+
+    private function updateCommittee(array $data): Organization
+    {
+        $organization = Organization::findOrFail($data['id']);
+
+        $organization->name = $data['name'];
+        $organization->followed_by = $data['followed_by_add'] ?? null;
+        $organization->save();
+
+        return $organization;
+    }
+
+    private function updateManagement(array $data): Organization
+    {
+        $organization = Organization::findOrFail($data['id']);
+
+        $organization->name = $data['name'];
+        $organization->followed_by_committee = $data['followed_by_committee_add'] ?? null;
+        $organization->save();
+
+        return $organization;
+    }
+
+    public function deleteOrganization(string $id): void
+    {
+        $organization = Organization::findOrFail($id);
+
+        match ($organization->type) {
+            'Министерство' => $this->deleteMinistry($id),
+            'Комитет' => $this->deleteCommittee($id),
+            'Управление' => $this->deleteManagement($id)
+        };
+    }
+
+    private function deleteMinistry(string $id): void
+    {
+        $organization = Organization::findOrFail($id);
+
+        $organization->delete();
+    }
+
+    private function deleteCommittee(string $id): void
+    {
+        $organization = Organization::findOrFail($id);
+
+        $organization->delete();
+    }
+
+    private function deleteManagement(string $id): void
+    {
+        $organization = Organization::findOrFail($id);
+
+        $organization->delete();
+    }
+}
