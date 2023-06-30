4c9bdd7700534c0e7a6c9ff7123ccf6d30325b9c
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-06-30 10:49:56 +0600

    Рефакторинг.

--- resources/views/organizations/search.blade.php
+++ resources/views/organizations/search.blade.php
@@ -8,13 +8,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-black">
-                    <div class="flex justify-center gap-6">
-                        <x-primary-button>
-                            <a href="{{ route('organizations.create') }}">{{ __('Создать организацию') }}</a>
-                        </x-primary-button>
-                    </div>
                     <div class="w-full mt-4">
-                        <form action="{{ route('organizations.search') }}" method="get">
+                        <form method="get">
                             <div class="flex justify-center">
                                 <input name="search" type="search" id="search"
                                        class="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -43,7 +38,7 @@
                                     Название организации
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Тип организации
+                                    Категория организации
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     Курирующая организация
@@ -54,51 +49,51 @@
                             </tr>
                             </thead>
                             <tbody>
-                            @foreach($organizations as $organization)
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black">
-                                        {{ $organization->name }}
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        {{ $organization->type }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        @if($organization->type === 'Управление')
-                                            {{ $organization->followedByCommittee ? $organization->followedByCommittee->name : 'Отсутствует' }}
-                                        @else
-                                            {{ $organization->followedBy ? $organization->followedBy->name : 'Отсутствует' }}
-                                        @endif
-                                    </td>
-                                    @if(\Illuminate\Support\Facades\Auth::id() === $organization->created_by)
+                                @foreach($organizations as $organization)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black">
+                                            {{ $organization->name }}
+                                        </th>
                                         <td class="px-6 py-4">
-                                            <x-primary-button>
-                                                <a href="{{ route('organizations.show', $organization->id) }}"
-                                                   class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Просмотреть</a>
-                                            </x-primary-button>
-                                            <x-primary-button class="mt-1">
-                                                <a href="{{ route('organizations.edit', $organization->id) }}"
-                                                   class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Изменить</a>
-                                            </x-primary-button>
-                                            <form class="mt-1 delete-form" action="{{ route('organizations.destroy', $organization->id) }}" method="post">
-                                                @csrf
-                                                @method('delete')
-                                                <x-primary-button class="delete-button"
-                                                                  data-type="{{ $organization->type }}"
-                                                                  data-text="{{ $organization->type === 'Министерство' ? 'Если вы удалите министерство, то за ним удалятся все привязанные к нему организации.'
+                                            {{ $organization->type }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            @if($organization->type === 'Управление')
+                                                {{ $organization->followedByCommittee ? $organization->followedByCommittee->name : 'Отсутствует' }}
+                                            @else
+                                                {{ $organization->followedBy ? $organization->followedBy->name : 'Отсутствует' }}
+                                            @endif
+                                        </td>
+                                        @if(\Illuminate\Support\Facades\Auth::id() === $organization->created_by)
+                                            <td class="px-6 py-4">
+                                                <x-primary-button>
+                                                    <a href="{{ route('organizations.show', $organization->id) }}"
+                                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Просмотреть</a>
+                                                </x-primary-button>
+                                                <x-primary-button class="mt-1">
+                                                    <a href="{{ route('organizations.edit', $organization->id) }}"
+                                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Изменить</a>
+                                                </x-primary-button>
+                                                <form class="mt-1 delete-form" action="{{ route('organizations.destroy', $organization->id) }}" method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <x-primary-button class="delete-button"
+                                                                      data-type="{{ $organization->type }}"
+                                                                      data-text="{{ $organization->type === 'Министерство' ? 'Если вы удалите министерство, то за ним удалятся все привязанные к нему организации.'
                                                                   : ($organization->type === 'Комитет' ? 'Если вы удалите комитет, то за ним удалятся все привязанные к нему организации.'
                                                                   : 'Вы уверены, что хотите удалить управление?') }}">
-                                                    Удалить
-                                                </x-primary-button>
-                                            </form>
-                                        </td>
-                                    @else
-                                        <td class="px-6 py-4">
-                                            <h1>У вас нет доступа к данной организации.</h1>
-                                        </td>
-                                    @endif
-                                </tr>
-                            @endforeach
+                                                        Удалить
+                                                    </x-primary-button>
+                                                </form>
+                                            </td>
+                                        @else
+                                            <td class="px-6 py-4">
+                                                <h1>У вас нет доступа к данной организации.</h1>
+                                            </td>
+                                        @endif
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
